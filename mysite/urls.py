ecc58c8fd16259583f84d0e70d9984e9946fb088
wsw0724 <wsw07240724@gmail.com>
2019-06-21 00:32:09 +0900

    urls.py test

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth import views#
+from django.contrib.auth import views######
 
 urlpatterns = [
     url(r'^admin', include(admin.site.urls)),
